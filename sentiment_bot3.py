385a63accc51cdf23f059f1c4981dbc714a1f515
nadiaBinetti <nadia.binetti@gmail.com>
2025-05-21 11:30:34 +0200

    Update sentiment_bot3.py

--- sentiment_bot3.py
+++ sentiment_bot3.py
@@ -40,7 +40,7 @@ def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": message}
     requests.post(url, data=payload)
-
+    print("Telegram response:", response.status_code, response.text)
 # ========== MODELLI ==========
 
 # FinBERT per news
